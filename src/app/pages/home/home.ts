74e9cce7eeb518c5f7e3eac99dd64ebe37cda46c
Juan Pablo Sánchez Brandán <jpsanchezbrandan@gmail.com>
2026-08-21 15:48:31 -0300

    fix: se corrigieron errores de implementacion de footer

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Navbar } from "../../shared/components/navbar/navbar";
+import { Footer } from '../../shared/components/footer/footer';
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar, RouterLink],
+  imports: [Navbar, RouterLink, Footer],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
