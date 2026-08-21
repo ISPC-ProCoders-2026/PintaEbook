95c4dc6576b3535ec1b049adeddc5148b0997bc5
Aubar48 <rociio.fressa@gmail.com>
2026-08-21 13:04:36 -0300

    feat: agregue dos logos transparentes en public/logos e hice el diseño provisorio del About

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { Navbar } from "../../shared/components/navbar/navbar";
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [Navbar, RouterLink],
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
-export class About {}
+export class About implements OnInit {
+  ngOnInit(): void {
+    this.checkScroll();
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    this.checkScroll();
+  }
+
+  private checkScroll(): void {
+    const reveals = document.querySelectorAll('.reveal');
+    reveals.forEach((el: Element) => {
+      const rect = el.getBoundingClientRect();
+      if (rect.top < window.innerHeight - 80) {
+        el.classList.add('active');
+      }
+    });
+  }
+}
